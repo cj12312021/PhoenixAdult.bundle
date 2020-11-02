5105da3673c560532551acf2723aa08ec683792a
Teda1 <72030708+Teda1@users.noreply.github.com>
2020-11-01 19:29:31 -0600

    naughty america tagline fix

--- Contents/Code/siteNaughtyAmerica.py
+++ Contents/Code/siteNaughtyAmerica.py
@@ -63,7 +63,8 @@ def update(metadata, siteID, movieGenres, movieActors):
     # Tagline and Collection(s)
     metadata.collections.clear()
     metadata.collections.add(metadata.studio)
-    metadata.collections.add(detailsPageElements['site'])
+    metadata.tagline = detailsPageElements['site']
+    metadata.collections.add(metadata.tagline)
 
     # Release Date
     date_object = datetime.fromtimestamp(detailsPageElements['published_at'])
@@ -111,7 +112,7 @@ def update(metadata, siteID, movieGenres, movieActors):
                 resized_image = Image.open(im)
                 width, height = resized_image.size
                 # Add the image proxy items to the collection
-                if width > 1:
+                if width < height:
                     # Item is a poster
                     metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
                 if width > 100 and width > height:
